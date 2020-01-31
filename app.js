6caf788b9f8b21cd0ec3d7efd052abb3e7df8f65
Caleb Duke <dukecj18@gmail.com>
2020-01-31 10:41:32 -0600

    update

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
     // console.log("ready");
+
+    let sentences = [
+        "Fall of the light through the ceiling fan Tryna catch it with your hand",
+        "Your fingers slip if the timing's bad We never knew how little time we had",
+        "Paint the haze on all the years All your hopes and all your fears",
+        "All the silly lies for love And you forget just what the reason was"
+    ]
+
+    let sentIndex = 0;
+    let letterIndex = 0;
+    let currentSentence = sentences[sentIndex];
+    let currentLetter = currentSentence[letterIndex];
+
+    $('#sentence').text(currentSentence);
+    $('#target-letter').text(currentLetter);
     $("#keyboard-upper-container").hide();
     
     
-    $(document).keydown(function(){
-        if (event.which == 16) {
+    $(document).keydown(function(e){
+        if (e.which == 16) {
             $("#keyboard-upper-container").show();
             $("#keyboard-lower-container").hide();
         }
 
     })
        
-    $(document).keyup(function(){
-        if (event.which == 16) {
+    $(document).keyup(function(e){
+        if (e.which == 16) {
             $("#keyboard-upper-container").hide();
             $("#keyboard-lower-container").show();
         }
@@ -25,13 +40,23 @@ $(document).ready(function() {
 
     $(document).keypress(function(e){
         let keyCharacter = $('#' + e.keyCode);
-        keyCharacter.css("background-color", "green");
+        keyCharacter.addClass('highlight');
         $(document).keyup(function(){
-            keyCharacter.css("background-color","white")
+            keyCharacter.removeClass('highlight')
         })
 
+        if(currentSentence.charCodeAt(letterIndex) === e.keyCode) {
+            $('#feedback').append('<span class="glyphicon glyphicon-ok"></span>');
+        }else{
+            $('#feedback').append('<span class="glyphicon glyphicon-remove"></span>');
+        }
+        $('#yellow-block').css('left', '+=17.5px');
+        letterIndex++;
+        currentLetter = currentSentence[letterIndex];
+        $('#target-letter').text(currentLetter);
+
     });
-  
+   
 
 
 
